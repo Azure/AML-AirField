34ecd14b9ed8ce1c7ac98c556cb1f438f8ec9345
Joon Lee <39318141+JoonLee225@users.noreply.github.com>
2018-07-17 13:03:05 -0700

    Fixed echo error in deploy.sh (#1)
    Fixed statement with parentheses to be recognized as string

--- Image/yolov3/deploy.sh
+++ Image/yolov3/deploy.sh
@@ -9,7 +9,7 @@ echo ''
 ./build_model.sh
 
 echo ''
-echo Creating Azure ML service (it takes a while)
+echo "Creating Azure ML service (it takes a while)"
 echo ''
 
 az ml service create realtime --model-file model --model-file data -f score.py -d app.py -d ui.html -n yolov3 -r python
